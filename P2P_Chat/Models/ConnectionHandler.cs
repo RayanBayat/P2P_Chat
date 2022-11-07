cca3748e123473bfea9a99be20d0c0b9b009b930
Rayan Bayat <rayan.bayat@hotmail.com>
2022-11-07 18:14:50 +0100

    somstuff

--- P2P_Chat/Models/ConnectionHandler.cs
+++ P2P_Chat/Models/ConnectionHandler.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -11,6 +13,7 @@ namespace P2P_Chat.Models
     public class ConnectionHandler
     {
         TcpClient? client;
+        TcpListener? server;
         public void sendMessage(String message)
         {
             // Here is the code which sends the data over the network.
@@ -19,7 +22,71 @@ namespace P2P_Chat.Models
         }
         public void connect(String ip, Int32 port)
         {
-            client = new TcpClient(ip,port);
+            try
+            {
+                client = new TcpClient(ip, port);
+            }
+            catch (SocketException e)
+            {
+                MessageBox.Show("No one is listening on IP " + ip + " and port " + port);
+               
+            }
+           
+        }
+        public void startListening(String ip, Int32 port)
+        {
+            try
+            {
+                IPAddress localAddr = IPAddress.Parse(ip);
+                server = new TcpListener(localAddr, port);
+                server.Start();
+            }
+            catch (SocketException e)
+            {
+                MessageBox.Show("Cannot listen to IP " + ip + " and port " + port);
+
+            }
+
+        }
+
+        public void senddata(String message)
+        {
+
+                try
+                {
+                    message += "\n";
+                    // Console.WriteLine(tcpc);
+                    // Translate the passed message into ASCII and store it as a Byte array.
+                    Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                    // Get a client stream for reading and writing.
+                    NetworkStream stream = client.GetStream();
+
+                    // Send the message to the connected TcpServer.
+                    stream.Write(data, 0, data.Length);
+                }
+                catch (ArgumentNullException e)
+                {
+                    Console.WriteLine("ArgumentNullException: {0}", e);
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("SocketException: {0}", e);
+                }
+            
+        }
+            private void listeningloop()
+        {
+            while (true)
+            {
+                Byte[] bytes = new Byte[8096];
+                String data = null;
+
+                client = server.AcceptTcpClient();
+                data = null;
+                NetworkStream stream = client.GetStream();
+            }
+
         }
 
     }
--- P2P_Chat/ViewModels/MainViewModel.cs
+++ P2P_Chat/ViewModels/MainViewModel.cs
@@ -2,7 +2,10 @@
 using P2P_Chat.ViewModels.Commands;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -10,33 +13,64 @@ using System.Windows.Input;
 
 namespace P2P_Chat.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel :INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private bool _popupActive;
         public ConnectionHandler Connection { get; set; }
         public String ToIP { get; set; }
         public Int32 port { get; set; }
         public String MessageToSend { get; set; }
         public ICommand PushCommand { get; set; }
         public ICommand ToIPCommand { get; set; }
+        public ICommand ListenCommand { get; set; }
+
+        public bool PopUpActive { get; set; }
+        //{
+        //    get { return _popupActive; }
+        //    set { _popupActive = value;
+        //     //   OnPropertyChanged("PopUpActive");
+        //    }
+        //}
 
         public MainViewModel(ConnectionHandler connectionHandler)
         {
             this.Connection = connectionHandler;
             this.ToIPCommand = new Connect(this);
             this.PushCommand = new SendMessageCommand(this);
-            
+            this.ListenCommand = new Listen(this);
+            PopUpActive = true;
         }
+
+       
 
 
 
         // to Model
         public void sendMessage()
         {
-            Connection.sendMessage(MessageToSend);
+            Connection.senddata(MessageToSend);
+            
         }
         public void establishConnection()
         {
             Connection.connect(ToIP, port);
         }
+        public void startListening()
+        {
+            Connection.startListening(ToIP, port);
+        }
+
+        //protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        //{
+        //    //if (propertyName == "Search")
+        //    //{
+        //    //    FilterSearch();
+        //    //}
+        //    if (PropertyChanged != null)
+        //    {
+        //        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        //    }
+        //}
     }
 }
